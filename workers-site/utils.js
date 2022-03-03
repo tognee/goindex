161315dd0433484bc3686900ff02e990d4af5b65
tognee <tognee64@gmail.com>
2022-03-03 22:40:22 +0100

    Added option to selfhost favicon and avatar

--- workers-site/utils.js
+++ workers-site/utils.js
@@ -2,7 +2,7 @@ import { uiConfig } from './config/ui.js'
 import { authConfig } from './config/auth.js'
 
 export function variableParser(html, current_drive_order = 0, model = {}){
-  return html.replace('{{ PAGE_DATA }}', `
+  let data = html.replace('{{ PAGE_DATA }}', `
 <script>
   window.drive_names = JSON.parse('${JSON.stringify(authConfig.roots.map(it => it.name))}');
   window.MODEL = JSON.parse('${JSON.stringify(model)}');
@@ -10,6 +10,13 @@ export function variableParser(html, current_drive_order = 0, model = {}){
   window.UI = JSON.parse('${JSON.stringify(uiConfig)}');
 </script>`)
   .replace('{{ PAGE_TITLE }}', authConfig.siteName)
-  .replace('{{ PAGE_FAVICON }}', authConfig.siteFavicon)
-  .replace('{{ PAGE_AVATAR }}', authConfig.siteAvatar)
+  if (authConfig.favicon)
+    data = data.replace('{{ PAGE_FAVICON }}', authConfig.siteFavicon)
+  else
+    data = data.replace('{{ PAGE_FAVICON }}', "/favicon.png")
+  if (authConfig.avatar)
+    data = data.replace('{{ PAGE_AVATAR }}', authConfig.siteAvatar)
+  else
+    data = data.replace('{{ PAGE_AVATAR }}', "/avatar.png")
+  return data
 }
